1a96f599019e363843d31450da8030eb599a9227
pjmantoss <joelptoss@gmail.com>
2020-04-16 17:02:00 +0100

    update script.js

--- script.js
+++ script.js
@@ -4,8 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const startBtn = document.querySelector('.start');
 
     const width = 10;
-    const currentIndex = 0; // first div in the grid
-    const appleIndex = 0;  // first div in the grid
-    const currentSnake = [2,1,0]; // 3rd div in the grid being 2 (or the HEAD), and 0 being the end (TAIL, with all 1's being the body fro now on)
-
+    let currentIndex = 0; // first div in the grid
+    let appleIndex = 0;  // first div in the grid
+    let currentSnake = [2,1,0]; // 3rd div in the grid being 2 (or the HEAD), and 0 being the end (TAIL, with all 1's being the body fro now on)
+    let direction = 1;
+    let score = 0;
+    let speed = 0.9;
+    let intervalTime = 0;
+    let interval = 0;
 })
